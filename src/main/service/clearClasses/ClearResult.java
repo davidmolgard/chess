3910ef8d234d259bbd5ee3475e557a4f1e6460cf
davidmolgard <molgarddavid@gmail.com>
2023-10-13 15:28:54 -0600

    implemented empty constructor

--- src/main/service/clearClasses/ClearResult.java
+++ src/main/service/clearClasses/ClearResult.java
@@ -4,6 +4,9 @@ public class ClearResult {
     private int responseCode;
     private String message;
 
+    public ClearResult() {
+        responseCode = 200;
+    }
     public ClearResult(int responseCode, String message) {
         this.responseCode = responseCode;
         this.message = message;
