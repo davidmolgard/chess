3cc828b54e51a97f69af9ef31f52d73d25a72f2e
davidmolgard <molgarddavid@gmail.com>
2023-10-13 14:53:10 -0600

    implemented request and result for Join

--- src/main/service/joinClasses/JoinRequest.java
+++ src/main/service/joinClasses/JoinRequest.java
@@ -1,5 +1,44 @@
 package service.joinClasses;
+import chess.ChessGame;
+import server.models.AuthToken;
 
 public class JoinRequest {
+    private AuthToken authToken;
+    private ChessGame.TeamColor color = null;
+    private int GameID;
 
+    public JoinRequest(AuthToken authToken, ChessGame.TeamColor color, int gameID) {
+        this.authToken = authToken;
+        this.color = color;
+        GameID = gameID;
+    }
+
+    public JoinRequest(AuthToken authToken, int gameID) {
+        this.authToken = authToken;
+        GameID = gameID;
+    }
+
+    public AuthToken getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(AuthToken authToken) {
+        this.authToken = authToken;
+    }
+
+    public ChessGame.TeamColor getColor() {
+        return color;
+    }
+
+    public void setColor(ChessGame.TeamColor color) {
+        this.color = color;
+    }
+
+    public int getGameID() {
+        return GameID;
+    }
+
+    public void setGameID(int gameID) {
+        GameID = gameID;
+    }
 }
--- src/main/service/joinClasses/JoinResult.java
+++ src/main/service/joinClasses/JoinResult.java
@@ -1,4 +1,31 @@
 package service.joinClasses;
 
 public class JoinResult {
+    private int responseCode;
+    private String message;
+
+    public JoinResult() {
+        responseCode = 200;
+    }
+
+    public JoinResult(int responseCode, String message) {
+        this.responseCode = responseCode;
+        this.message = message;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
