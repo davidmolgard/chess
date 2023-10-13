d7239b1c2228fbd7603b9d90bb30bc6aaf301fc3
davidmolgard <molgarddavid@gmail.com>
2023-10-13 14:33:14 -0600

    implemented request and result for List

--- src/main/service/listClasses/ListRequest.java
+++ src/main/service/listClasses/ListRequest.java
@@ -1,4 +1,18 @@
 package service.listClasses;
 
+import server.models.AuthToken;
+
 public class ListRequest {
+    private AuthToken authToken;
+    public ListRequest(AuthToken authToken) {
+        this.authToken = authToken;
+    }
+
+    public AuthToken getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(AuthToken authToken) {
+        this.authToken = authToken;
+    }
 }
--- src/main/service/listClasses/ListResult.java
+++ src/main/service/listClasses/ListResult.java
@@ -1,4 +1,27 @@
 package service.listClasses;
 
 public class ListResult {
+    private int responseCode;
+    private String message;
+
+    public ListResult(int responseCode, String message) {
+        this.responseCode = responseCode;
+        this.message = message;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
