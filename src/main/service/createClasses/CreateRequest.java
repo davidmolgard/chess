2c0c98d4f8fb4653b82797a02804c501bb1cc69d
davidmolgard <molgarddavid@gmail.com>
2023-10-13 14:44:11 -0600

    implemented request and result for Create

--- src/main/service/createClasses/CreateRequest.java
+++ src/main/service/createClasses/CreateRequest.java
@@ -1,4 +1,29 @@
 package service.createClasses;
 
+import server.models.AuthToken;
+
 public class CreateRequest {
+    private AuthToken authToken;
+    private String gameName;
+
+    public CreateRequest(AuthToken authToken, String gameName) {
+        this.authToken = authToken;
+        this.gameName = gameName;
+    }
+
+    public AuthToken getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(AuthToken authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
 }
--- src/main/service/createClasses/CreateResult.java
+++ src/main/service/createClasses/CreateResult.java
@@ -1,4 +1,41 @@
 package service.createClasses;
 
 public class CreateResult {
+    private String message;
+    private int responseCode;
+    private int GameID;
+
+    public CreateResult(int gameID) {
+        GameID = gameID;
+        responseCode = 200;
+    }
+
+    public CreateResult(int responseCode, String message) {
+        this.message = message;
+        this.responseCode = responseCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public int getGameID() {
+        return GameID;
+    }
+
+    public void setGameID(int gameID) {
+        GameID = gameID;
+    }
 }
