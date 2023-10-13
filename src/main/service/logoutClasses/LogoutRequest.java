630920a3248fee81a0d59f01d0795f442576f9d0
davidmolgard <molgarddavid@gmail.com>
2023-10-13 14:06:17 -0600

    implemented request result classes for logout

--- src/main/service/logoutClasses/LogoutRequest.java
+++ src/main/service/logoutClasses/LogoutRequest.java
@@ -1,4 +1,19 @@
 package service.logoutClasses;
 
+import server.models.AuthToken;
+import service.loginClasses.LoginRequest;
+
 public class LogoutRequest {
+    AuthToken authToken;
+
+    public LogoutRequest(AuthToken authToken) {
+        this.authToken = authToken;
+    }
+    public AuthToken getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(AuthToken authToken) {
+        this.authToken = authToken;
+    }
 }
--- src/main/service/logoutClasses/LogoutResult.java
+++ src/main/service/logoutClasses/LogoutResult.java
@@ -1,4 +1,20 @@
 package service.logoutClasses;
 
 public class LogoutResult {
+    String message;
+
+    public LogoutResult() {
+        message = null;
+    }
+
+    public LogoutResult(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
