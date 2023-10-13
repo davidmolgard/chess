c4bf0a7ee7b7c52b2effdf704cf7ead208ec148b
davidmolgard <molgarddavid@gmail.com>
2023-10-13 13:41:26 -0600

    created getters and setters for models

--- src/main/server/models/AuthToken.java
+++ src/main/server/models/AuthToken.java
@@ -3,4 +3,19 @@ package server.models;
 public class AuthToken {
     private String authToken;
     private String username;
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- src/main/server/models/Game.java
+++ src/main/server/models/Game.java
@@ -8,4 +8,43 @@ public class Game {
     private String blackUsername;
     private String gameName;
     private ChessGame game;
+    public int getGameID() {
+        return GameID;
+    }
+
+    public void setGameID(int gameID) {
+        GameID = gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public void setWhiteUsername(String whiteUsername) {
+        this.whiteUsername = whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public void setBlackUsername(String blackUsername) {
+        this.blackUsername = blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public void setGame(ChessGame game) {
+        this.game = game;
+    }
 }
