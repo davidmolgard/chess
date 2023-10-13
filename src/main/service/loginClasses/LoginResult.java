04c1fc79b0aab548940b9735799fa6049ac152b4
davidmolgard <molgarddavid@gmail.com>
2023-10-13 13:57:32 -0600

    implemented request result classes for login

--- src/main/service/loginClasses/LoginResult.java
+++ src/main/service/loginClasses/LoginResult.java
@@ -1,4 +1,42 @@
 package service.loginClasses;
 
+import server.models.AuthToken;
+
 public class LoginResult {
+    private String message;
+    private String username;
+    private AuthToken authToken;
+
+    public LoginResult(String errorMessage) {
+        message = errorMessage;
+    }
+
+    public LoginResult(String username, AuthToken authToken) {
+        this.username = username;
+        this.authToken = authToken;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public AuthToken getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(AuthToken authToken) {
+        this.authToken = authToken;
+    }
 }
