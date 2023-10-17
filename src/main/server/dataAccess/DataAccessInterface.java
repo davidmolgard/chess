66284a243e56295b0d22f5c130d25cc38d8232d4
davidmolgard <molgarddavid@gmail.com>
2023-10-17 17:42:54 -0600

    Javadoc updates

--- src/main/server/dataAccess/DataAccessInterface.java
+++ src/main/server/dataAccess/DataAccessInterface.java
@@ -5,6 +5,10 @@ import server.models.Game;
 
 import java.util.HashMap;
 
+/**
+ * DataAccessInterface allows for data to be inserted and returned from the database
+ * Implementations for games, users, and authorization tokens
+ */
 public interface DataAccessInterface {
     /**
      * Allows Data to be inserted to the database
@@ -13,19 +17,38 @@ public interface DataAccessInterface {
     int insert();
 
     /**
-     *
-     * @param gameID
-     * @return
-     * @throws DataAccessException
+     * @param gameID of game to find
+     * @return Game if found
+     * @throws DataAccessException if GameId is invalid/ game not found
      */
     Game find(int gameID) throws DataAccessException;
 
+    /**
+     * @return String representation of data type being accessed
+     */
     String findAll();
 
+    /**
+     * claims a spot in the game for the user provided
+     * @param gameID of game to join
+     * @param username of user looking to join the game
+     * @throws DataAccessException if user or game not able to be found
+     */
     void claimSpot(int gameID, String username) throws DataAccessException;
 
+    /**
+     * renames a game
+     * @param gameID to be renamed
+     * @param newName of game
+     * @throws DataAccessException if game cannot be found
+     */
     void updateGame(int gameID, String newName) throws DataAccessException;
 
+    /**
+     * removes a game
+     * @param gameID of game to remove
+     * @throws DataAccessException if game cannot be found
+     */
     void remove(int gameID) throws DataAccessException;
 
     /**
--- src/main/service/Services.java
+++ src/main/service/Services.java
@@ -15,31 +15,63 @@ import service.logoutClasses.LogoutResult;
 import service.registerClasses.RegisterRequest;
 import service.registerClasses.RegisterResult;
 
+/**
+ * Services contains methods for all services offered by the API
+ */
 public class Services {
+    /**
+     * clears the database if authorized to do so
+     * @param req @see ClearRequest
+     * @return @see ClearResult (includes error code if error occurred)
+     */
     ClearResult clear(ClearRequest req) {
         return new ClearResult();
     }
-
+    /**
+     * creates game if authorized to do so
+     * @param req @see CreateRequest
+     * @return @see CreateResult (includes error code if error occurred)
+     */
     CreateResult create(CreateRequest req) {
         return new CreateResult(0);
     }
-
+    /**
+     * joins game if authorized to do so
+     * @param req @see JoinRequest
+     * @return @see JoinResult (includes error code if error occurred)
+     */
     JoinResult join(JoinRequest req) {
         return new JoinResult();
     }
-
+    /**
+     * Lists all games if authorized to do so
+     * @param req @see ListRequest
+     * @return @see ListResult (includes error code if error occurred)
+     */
     ListResult list(ListRequest req) {
         return new ListResult(200, "");
     }
-
+    /**
+     * Login user if authorized to do so
+     * @param req @see LoginRequest
+     * @return @see LoginResult (includes error code if error occurred)
+     */
     LoginResult login(LoginRequest req) {
         return new LoginResult(0, "");
     }
-
+    /**
+     * Logout user if authorized to do so
+     * @param req @see LogoutRequest
+     * @return @see LogoutResult (includes error code if error occurred)
+     */
     LogoutResult logout(LogoutRequest req) {
         return new LogoutResult();
     }
-
+    /**
+     * registers user if authorized to do so
+     * @param req @see RegisterRequest
+     * @return @see RegisterResult (includes error code if error occurred)
+     */
     RegisterResult register(RegisterRequest req) {
         return new RegisterResult(0, "");
     }
