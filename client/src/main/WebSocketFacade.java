122ba4d4ddbc4f0d95afdf66a742f4824cfec756
davidmolgard <molgarddavid@gmail.com>
2023-12-13 14:57:56 -0700

    Created WebSocket Facade for client

---
+++ client/src/main/WebSocketFacade.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+//need to extend Endpoint for websocket to work properly
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+
+
+    public WebSocketFacade() throws Exception {
+        URI uri = new URI("ws://localhost:8080/connect");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    //Endpoint requires this method, but you don't have to do anything
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+}
